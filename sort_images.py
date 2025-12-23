fcb87a9a9fddebc7e2657ff66f17da2f846e63d3
Pavel D <cz8pru@seznam.cz>
2025-12-23 09:40:25 +0100

    Remove empty dire

--- sort_images.py
+++ sort_images.py
@@ -30,143 +30,170 @@ class CustomFormatter(logging.Formatter):
         log_fmt = self.FORMATS.get(record.levelno)
         formatter = logging.Formatter(log_fmt)
         return formatter.format(record)
-	
+  
 
 
 
 
 def mkdir_custom(path, dry_run=False):
-	if dry_run:
-		logger.debug(f'Make directory: {path}')
-		return
-	else:
-		os.makedirs(path, exist_ok=True)
+  if dry_run:
+    logger.debug(f'Make directory: {path}')
+    return
+  else:
+    os.makedirs(path, exist_ok=True)
 
 
 def move_custom(src, dst, dry_run=False):
-	if dry_run:
-		logger.debug(f'Move file from {src} to {dst}')
-		return
-	else:
-		shutil.move(src, dst)
+  if dry_run:
+    logger.debug(f'Move file from {src} to {dst}')
+    return
+  else:
+    shutil.move(src, dst)
 
 
 def get_file_img(f):
-	if f.endswith(".jpg") or f.endswith(".jpeg") or f.endswith(".JPG") or f.endswith(".JPEG"):
-		return True
-	return False	
+  if f.endswith(".jpg") or f.endswith(".jpeg") or f.endswith(".JPG") or f.endswith(".JPEG"):
+    return True
+  return False	
 
 def get_file_video(f):
-	if f.endswith(".mp4") or f.endswith(".avi") or f.endswith(".mov") or f.endswith(".mkv"):
-		return True
-	return False
+  if f.endswith(".mp4") or f.endswith(".avi") or f.endswith(".mov") or f.endswith(".mkv"):
+    return True
+  return False
 
 def get_images(input_images_path, images_limit=10000, eadir_remove=False):
-	max_processed_images = 0
+  max_processed_images = 0
 
-	# Recursively walk through all subdirectories and store the path + name of the jpg images
-	images = []
-	for root, dirs, files in os.walk(input_images_path):
-		
-		if "@eaDir" in root:
-			logger.debug(f"Skipping directory {root} because it is an @eaDir.")
-			if eadir_remove:
-				try:
-					shutil.rmtree(root)
-					logger.info(f"Removed @eaDir directory: {root}")
-				except Exception as e:
-					logger.error(f"Failed to remove @eaDir directory {root}: {e}")
-			continue
+  # Recursively walk through all subdirectories and store the path + name of the jpg images
+  images = []
+  for root, dirs, files in os.walk(input_images_path):
+    
+    if "@eaDir" in root:
+      logger.debug(f"Skipping directory {root} because it is an @eaDir.")
+      if eadir_remove:
+        try:
+          shutil.rmtree(root)
+          logger.info(f"Removed @eaDir directory: {root}")
+        except Exception as e:
+          logger.error(f"Failed to remove @eaDir directory {root}: {e}")
+      continue
 
-		for f in files:
-			# I'm only interested in pictures
-			if get_file_img(f) or get_file_video(f):
-				tmp = os.path.join(root, f)
-				images.append(tmp)
-				logger.debug(tmp)
-				max_processed_images += 1
+    for f in files:
+      # I'm only interested in pictures
+      if get_file_img(f) or get_file_video(f):
+        tmp = os.path.join(root, f)
+        images.append(tmp)
+        logger.debug(tmp)
+        max_processed_images += 1
 
-				if max_processed_images > images_limit:
-					logger.warning(f"Maximum number of images to process reached: {max_processed_images}.")
-					return images
-	return images
+        if max_processed_images > images_limit:
+          logger.warning(f"Maximum number of images to process reached: {max_processed_images}.")
+          return images
+  return images
 
 
 def main(input_images_path, output_images_path="./tmp", dry_run=False, eadir_remove=False):
-	'''
-	Sorts images by date taken, using EXIF tags.
-	Images are moved to a folder with the format YYYY.MM.DD.
-	Images without EXIF tags are moved to a folder named 0000.
+  '''
+  Sorts images by date taken, using EXIF tags.
+  Images are moved to a folder with the format YYYY.MM.DD.
+  Images without EXIF tags are moved to a folder named 0000.
 
-	:param input_images_path: Path where images to be organized are located.
-	'''
+  :param input_images_path: Path where images to be organized are located.
+  '''
 
-	fail_count = 0
-	success_count = 0
-	processed_images = 0
+  fail_count = 0
+  success_count = 0
+  processed_images = 0
 
-	# Path for sorted files to be stored
-	# If it doesn't exist, creates a new one
-	if not os.path.exists(output_images_path):
-		os.mkdir(output_images_path)
-
-
-	images = get_images(input_images_path, eadir_remove=eadir_remove)
-
-	# Extracts the date an image was taken and moves it to a folder with the format YYYY.MM.DD
-	# If the image doesn't have EXIF tags, sends it to a folder named 0000
-	for img in images:
-		processed_images += 1
-		date_path = "NEEDS_MANUAL_SORTING"
-		if get_file_img(img) or get_file_video(img):
-			with open(img, "rb") as file:
-				tags = exifread.process_file(file, details=False, stop_tag="DateTimeOriginal")
-				
-				try:
-					date_list = str(tags["EXIF DateTimeOriginal"])[:10].split(":")
-					date_path = os.path.join(date_list[0], date_list[1], date_list[2])
-					logger.debug(f"Image {img} has EXIF tags. Date: {date_path}")
-				except:
-					logger.warning(f"Image {img} does not have EXIF tags.")
-					
-					# Try to extract date from filename using regex
-					match = re.search(
-						r'(19[0-9]{2}|20[0-9]{2})'  # year: 1900-2099
-						r'(0[1-9]|1[0-2])'          # month: 01-12
-						r'(0[1-9]|[12][0-9]|3[01])',# day: 01-31
-						os.path.basename(img)
-					)
-					if match:
-						date_path = os.path.join(match.group(1), match.group(2), match.group(3))
-						logger.debug(f"File {img} date extracted from filename: {date_path}")
-						success_count += 1
-					else:
-						logger.warning(f"File {img} does not match date pattern in filename.")
-						fail_count += 1
+  # Path for sorted files to be stored
+  # If it doesn't exist, creates a new one
+  if not os.path.exists(output_images_path):
+    os.mkdir(output_images_path)
 
 
+  images = get_images(input_images_path, eadir_remove=eadir_remove)
+
+  # Extracts the date an image was taken and moves it to a folder with the format YYYY.MM.DD
+  # If the image doesn't have EXIF tags, sends it to a folder named 0000
+  for img in images:
+    processed_images += 1
+    date_path = "NEEDS_MANUAL_SORTING"
+    if get_file_img(img) or get_file_video(img):
+      with open(img, "rb") as file:
+        tags = exifread.process_file(file, details=False, stop_tag="DateTimeOriginal")
+        
+        try:
+          date_list = str(tags["EXIF DateTimeOriginal"])[:10].split(":")
+          date_path = os.path.join(date_list[0], date_list[1], date_list[2])
+          logger.debug(f"Image {img} has EXIF tags. Date: {date_path}")
+        except:
+          logger.warning(f"Image {img} does not have EXIF tags.")
+          
+          # Try to extract date from filename using regex
+          match = re.search(
+            r'(19[0-9]{2}|20[0-9]{2})'  # year: 1900-2099
+            r'(0[1-9]|1[0-2])'          # month: 01-12
+            r'(0[1-9]|[12][0-9]|3[01])',# day: 01-31
+            os.path.basename(img)
+          )
+          if match:
+            date_path = os.path.join(match.group(1), match.group(2), match.group(3))
+            logger.debug(f"File {img} date extracted from filename: {date_path}")
+            success_count += 1
+          else:
+            logger.warning(f"File {img} does not match date pattern in filename.")
+            fail_count += 1
 
 
 
 
 
 
-		dest_image = os.path.basename(img)
-
-		destination = os.path.join(output_images_path, date_path, dest_image)
-		
-		mkdir_custom(os.path.join(output_images_path, date_path), dry_run)
-		move_custom(img, destination, dry_run)
-
-		logger.info(f"Processed {processed_images}/{len(images)} images. ({img} -> {destination})")
 
 
-	print("="*120 + "\n\n")
-	logger.info(f"Sorted         {success_count:>10} files.")
-	logger.info(f"Failed to sort {fail_count:>10} files.")
-	print("="*120)
-	logger.info(f"Total          {processed_images:>10} files in total.")
+    dest_image = os.path.basename(img)
 
+    destination = os.path.join(output_images_path, date_path, dest_image)
+    
+    mkdir_custom(os.path.join(output_images_path, date_path), dry_run)
+    move_custom(img, destination, dry_run)
+
+    logger.info(f"Processed {processed_images}/{len(images)} images. ({img} -> {destination})")
+
+
+  print("="*120 + "\n\n")
+  logger.info(f"Sorted         {success_count:>10} files.")
+  logger.info(f"Failed to sort {fail_count:>10} files.")
+  print("="*120)
+  logger.info(f"Total          {processed_images:>10} files in total.")
+
+
+
+def remove_empty_dirs(path, dry_run=False):
+    """
+    Recursively remove empty directories.
+    """
+
+    try:
+      for root, dirs, files in os.walk(path, topdown=False):
+        # Only remove directories that have no files and no subdirectories
+        for directory in dirs:
+          dir_path = os.path.join(root, directory)
+          try:
+            # Check if directory is completely empty (no files, no subdirs)
+            if not any(os.scandir(dir_path)):
+              if dry_run:
+                logger.debug(f'Remove empty directory: {dir_path}')
+              else:
+                os.rmdir(dir_path)
+                logger.info(f"Removed empty directory: {dir_path}")
+            else:
+              logger.debug(f"Directory not empty, skipping: {dir_path}")
+          except Exception as e:
+            logger.debug(f"Could not remove directory {dir_path}: {e}")
+
+    except Exception as e:
+        logger.error(f"Error removing empty directories: {e}")
 
 
 logger = logging.getLogger(__name__)
@@ -177,10 +204,10 @@ logger.setLevel(logging.INFO)
 
 
 parser = argparse.ArgumentParser(
-	prog='sort_images',
-	description='A simple python script to sort pictures by date taken.',
-	epilog='Originaly from https://github.com/vitords/sort-images/'
-	)
+  prog='sort_images',
+  description='A simple python script to sort pictures by date taken.',
+  epilog='Originaly from https://github.com/vitords/sort-images/'
+  )
 parser.add_argument('input_images_path')
 parser.add_argument('output_images_path', nargs='?', default="./tmp", help='Path where sorted images will be stored. Default is "./tmp".')
 parser.add_argument('-v', '--verbose', action='store_true')  # on/off flag
@@ -196,9 +223,12 @@ logger.info(f"dry_run: {args.dry_run}")
 logger.info(f"eadir_remove: {args.eadir_remove}")
 
 if (args.verbose):
-	logger.setLevel(logging.DEBUG)
+  logger.setLevel(logging.DEBUG)
 
 
 
 
 main(args.input_images_path, args.output_images_path, args.dry_run, args.eadir_remove)
+
+# Remove empty directories
+remove_empty_dirs(args.input_images_path, args.dry_run)
